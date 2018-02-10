efe632b7d62dc44680f34cc1c3a5cdd818808441
Brock Atkinson <brockatkinson@hotmail.com>
2018-02-11 10:44:13 +1100

    implement hacky delete

--- app/src/init.js
+++ app/src/init.js
@@ -8,6 +8,16 @@ function init(name, ui) {
   require('view/app');
   var Gun = require('gun');
   Gun.chain.convert = require('rounds');
+
+  const rel_ = Gun.val.rel._;  // '#'
+  const node_ = Gun.node._;  // '_'
+
+  Gun.chain.unset = function(node){
+    if( this && node && node[node_] && node[node_].put && node[node_].put[node_] && node[node_].put[node_][rel_] )
+      this.put( { [node[node_].put[node_][rel_]]:null} );
+    return this;
+  }
+
   var opts = {
     db: new Gun()
   };
