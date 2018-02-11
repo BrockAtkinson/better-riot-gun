0462e89c2a67d19b7ed03906043e8f6b875ed9fc
Brock Atkinson <brockatkinson@hotmail.com>
2018-02-11 12:27:07 +1100

    create separate deleted list

--- app/src/init.js
+++ app/src/init.js
@@ -9,15 +9,6 @@ function init(name, ui) {
   var Gun = require('gun');
   Gun.chain.convert = require('rounds');
 
-  const rel_ = Gun.val.rel._;  // '#'
-  const node_ = Gun.node._;  // '_'
-
-  Gun.chain.unset = function(node){
-    if( this && node && node[node_] && node[node_].put && node[node_].put[node_] && node[node_].put[node_][rel_] )
-      this.put( { [node[node_].put[node_][rel_]]:null} );
-    return this;
-  }
-
   var opts = {
     db: new Gun()
   };
